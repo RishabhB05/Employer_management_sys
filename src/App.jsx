90576e1ecc638503134fbe2578f7997ccfd6a61b
spywolf2049 <rishabhbaruah87@gmail.com>
2025-06-15 15:56:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './components/Auth/login'; // Path is correct if file exists
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import AdminTaskView from './pages/AdminTaskView';
+import { getLocalStorage, setLocalStorage } from './pages/utils/localStorage';
 
 const App = () => {
+
+  useEffect(()=>{
+    // setLocalStorage()
+    getLocalStorage();
+
+  },)
+
+
   return (
     <>
-      {/* <Login/>   */}
-      <EmployeeDashboard/>
+      <Login/>  
+      {/* <EmployeeDashboard/> */}
+      {/* <AdminDashboard/> */}
+      {/* <AdminTaskView/> */}
     </>
   );
 };
